08de241c8f2af0d346b67a66e567dac5f8822ef0
beauhelmi <beauhelmi@gmail.com>
2022-11-11 10:03:20 +0800

    adding two new places

--- src/data.jsx
+++ src/data.jsx
@@ -11,5 +11,29 @@ export default [
         "Ipoh, tempat aku dilahirkan. Untuk tahun ini juga banyak mengimbau kenangan di sini pada trip kali ini. Hadir ke kenduri kahwin anak Ayah Ngah iaitu Atun dan juga pulang beraya dengan episod sedih terpaksa beraya di Homestay sahaja gara-gara Aidan Fuzi demam. Begitulah kenanganku pada tahun ini",
         imageUrl: "../public/ipoh.jpg",
     },
+    {
+        id: 1,
+        tajuk: "Kuala Kangsar",
+        class: "kuala kangsar",
+        lokasi: "Perak",
+        googleMapsUrl: "https://www.google.com",
+        tarikhMula: "26 Okt 2022",
+        tarikhHabis: "23 Dec 2022",
+        konteks: 
+        "Kampung halaman Isteri ku, untuk tahun ini tidak banyak singgah disini. Tapi kami ada pulang sebentar menikmati laksa, cendol dan juga macam-macam aneka makanan yang lain. Sepertinya pulang sebentar, menziarahi kubur juga adalah adat keluarga Isteriku",
+        imageUrl: "../public/kk.jpg",
+    },
+    {
+        id: 1,
+        tajuk: "Melaka",
+        class: "melaka",
+        lokasi: "Melaka",
+        googleMapsUrl: "https://www.google.com",
+        tarikhMula: "20 Nov 2022",
+        tarikhHabis: "23 Nov 2022",
+        konteks: 
+        "Perjalanan ke Melaka tidak sah jika tidak singgah untuk menikmati asam pedas. Untuk trip kali ini, aku dan ibuku ada singgah bersarapan di kedai pakcik ku bernama Zulkifly di Mini Malaysia Melaka, hidangan yang beliau sajikan pada kami adalah seumpama makanan di syurga, dengai roti canai kambingnya dan teh ais istimewa!",
+        imageUrl: "../public/melaka.jpg",
+    },
 ];
 
